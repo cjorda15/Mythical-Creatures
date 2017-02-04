244254ff80486038e77a0f22f9fd1f083a58ff6b
Chris Jordan <cjorda15@msudenver.edu>
2017-02-04 14:48:29 -0700

    All .js files will pass test now

--- medusa.js
+++ medusa.js
@@ -3,26 +3,20 @@ function Medusa(name){
   this.statues=[];
   this.stare = function(victim) {
   victim.stoned=true;
-  this.statues=[victim]
-  if (this.statues.length===4) {
-var lucky= this.statues[0];
-delete this.statues[0];
-lucky.stoned=false;
-
+  if (this.statues.length===3) {
+    var lucky= this.statues[0];
+    lucky.stoned=false;
+    this.statues.slice(0,1)
+  }else{
+          this.statues.push(victim)
+    }
   }
-
-
 }
-}
-
-
-
 
 function Person(name){
   this.name=name;
   this.stoned = false;
-};
-
+}
 
 module.exports.Person=Person;
 module.exports.Medusa=Medusa;
