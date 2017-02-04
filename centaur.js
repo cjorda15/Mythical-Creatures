e494af6ec7a4fbfce6c815e4d037cc782d448655
Chris Jordan <cjorda15@msudenver.edu>
2017-02-04 14:17:05 -0700

    centaur.js now will pass the test

--- centaur.js
+++ centaur.js
@@ -4,6 +4,7 @@ this.breed = breed;
 this.work = 0;
 this.cranky = false;
 this.standing = true;
+this.layingDown = false;
 }
 Centaur.prototype.shoot = function () {
     if(this.cranky === true) {
@@ -31,11 +32,33 @@ Centaur.prototype.shoot = function () {
   };
 
   Centaur.prototype.sleep = function () {
+    if(this.layingDown===true){
+      this.cranky =false;
+      return "ZZZZ"
+    }else{
     return "NO!"
   };
+}
+  Centaur.prototype.layDown= function(){
+    this.standing = false;
+    this.layingDown = true;
+  }
 
+  Centaur.prototype.standUp= function(){
+    this.standing = true;
+    this.layingDown = false;
+  }
 
+ Centaur.prototype.drinkPotion=function() {
+ if(this.standing===true && this.cranky===true){
+     this.cranky=false;
+   }else if(this.standing===true && this.cranky===false){
+     this.cranky=true;
+   }else if(this.layingDown===true){
+     return 'Not while I\'m standing!'
 
+ }
+ }
 
 
 module.exports = Centaur;
